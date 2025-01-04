3ebabe52ae68ae4cf3b9be1dbfe12629b4370fab
lee <skystar9927@naver.com>
2025-01-04 23:52:29 +0900

    헤더ui구현

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,9 +1,34 @@
+import styled from "styled-components";
+
 function Header() {
   return (
-    <div>
-      header
-    </div>
+    <HeaderWrapper>
+      <HeaderStart>leeemingyu</HeaderStart>
+      <HeaderEnd>
+        <HeaderEndItem>About</HeaderEndItem>
+        <HeaderEndItem>Skills</HeaderEndItem>
+        <HeaderEndItem>Projects</HeaderEndItem>
+        <HeaderEndItem>Contact</HeaderEndItem>
+      </HeaderEnd>
+    </HeaderWrapper>
   );
 }
-
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: #010409;
+  padding: 24px;
+`;
+const HeaderStart = styled.div`
+  color: #f0f6fc;
+`;
+const HeaderEnd = styled.div`
+  display: flex;
+  color: #f0f6fc;
+  width: 250px;
+  justify-content: space-between;
+`;
+const HeaderEndItem = styled.div`
+  
+`;
 export default Header;
