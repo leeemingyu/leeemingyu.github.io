974148d70bc9c57702edf8ddaab956675d855bcf
lee <skystar9927@naver.com>
2025-01-05 23:27:11 +0900

    헤더 디자인 수정

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -20,15 +20,34 @@ const HeaderWrapper = styled.div`
   padding: 24px;
 `;
 const HeaderStart = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+  border-radius: 5px;
   color: #f0f6fc;
+  cursor:pointer;
+  padding: 0 6px 0 6px;
+  height: 30px;
+  &:hover{
+    background-color:rgb(37, 42, 50);
+  }
 `;
 const HeaderEnd = styled.div`
   display: flex;
-  color: #f0f6fc;
+  color:rgb(210, 210, 210);
   width: 250px;
   justify-content: space-between;
 `;
 const HeaderEndItem = styled.div`
-  
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+  border-radius: 5px;  
+  cursor:pointer;
+  padding: 0 6px 0 6px;
+  &:hover{
+    background-color:rgb(37, 42, 50);
+    color: #f0f6fc;
+  }
 `;
 export default Header;
