85a29db12341bcae8366cbcbf58824f932c4de80
lee <skystar9927@naver.com>
2025-01-06 17:14:01 +0900

    푸터초기ui구현

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,18 @@
+import styled from "styled-components";
+
 function Footer() {
   return (
-    <div>
-      footer
-    </div>
+    <FooterWrapper>
+      &copy; 2025 leeemingyu. All rights reserved.
+    </FooterWrapper>
   );
 }
+const FooterWrapper = styled.div`
+  background-color:rgb(11, 6, 30);
+  color:rgb(255, 255, 255);
+  display: flex;
+  justify-content: center;
+  height: 64px;
+`;
 
 export default Footer;
